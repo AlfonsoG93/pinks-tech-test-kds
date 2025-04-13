aaab1238b95ab777ac8f9aba7d0c06be1af526d9
Alfonso Moncada <alfonso.moncada@osapiens.com>
2025-04-13 23:39:55 +0200

    -patch enhance drag UI allow scroll in DELIVERED
     - Disable dragging action if order is delivered, allow scrolling if column is overflowing
     - modify to add dragging cursor class only in orders not delivered

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import s from "@/components/Card/Card.module.scss";
 import {Order} from "@/dtos/Order.dto";
 import {useState} from "react";
+import {OrderState} from "@/enums/OrderState";
 
 type CardProps = {
 	order: Order;
@@ -12,14 +13,16 @@ export default function Card({ order, onClick }: CardProps ) {
 	
 	return (
 		<div
-			draggable
+			draggable={order.state !== OrderState.DELIVERED}
 			onDragStart={(e) => {
-				e.dataTransfer.setData("text/plain", order.id);
-				setIsDragging(true)
+				if (order.state !== OrderState.DELIVERED) {
+					e.dataTransfer.setData("text/plain", order.id);
+					setIsDragging(true);
+				} else return;
 			}}
 			onDragEnd={() => setIsDragging(false)}
 			onClick={() => onClick && onClick(order.id)}
-			className={`${s["pk-card"]} ${isDragging ? s["pk-card__dragging"] : ""}`}
+			className={`${s["pk-card"]} ${order.state !== OrderState.DELIVERED ? s["pk-card__grababble"] : ""} ${isDragging ? s["pk-card__dragging"]: ""}`}
 		>
 			<div>
             <span>
